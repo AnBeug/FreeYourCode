9f312d5c4f4172ab24cd8ddab68a07800f1b0b3c
Peter Graham <peterghm@gmail.com>
2013-07-24 19:33:17 -0700

    Set some global and development-specific settings

--- server.rb
+++ server.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 
 app = Sinatra.new do
+  configure do
+    set :sessions, true
+    set :logging, true
+  end
+
+  configure(:development) do
+    set :dump_errors, true
+    set :show_exceptions, true
+  end
+
   get '/' do
     "It works!"
   end
