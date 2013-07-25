c8d479587ce1018db4e10ed8fd83af700efff596
Peter Graham <peterghm@gmail.com>
2013-07-25 01:20:16 -0700

    Github omniauth finds or creates a User

--- Gemfile
+++ Gemfile
@@ -4,3 +4,8 @@ ruby "1.9.3"
 gem "sinatra"
 gem "newrelic_rpm"
 gem 'unicorn-heroku'
+gem 'activerecord', '~> 3.2.12'
+gem 'sinatra-activerecord'
+gem 'pg'
+gem "omniauth"
+gem "omniauth-github"
--- server.rb
+++ server.rb
@@ -1,4 +1,16 @@
 require 'sinatra'
+require 'active_record'
+require 'omniauth'
+require 'omniauth-github'
+
+ENV['RACK_ENV'] ||= 'development'
+
+files = []
+files.concat(Dir[File.join(File.dirname(__FILE__), 'config','**','*.rb')])
+files.concat(Dir[File.join(File.dirname(__FILE__), 'models','**','*.rb')])
+files.each do |file|
+  require file
+end
 
 class App < Sinatra::Base
   configure do
@@ -19,8 +31,32 @@ class App < Sinatra::Base
     set :session_secret, ENV['SESSION_KEY']
   end
 
+  use OmniAuth::Builder do
+    provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
+  end
+
   get '/' do
     erb :index
   end
-end
 
+  get '/auth/:name/callback' do
+    auth = request.env["omniauth.auth"]
+
+    user = User.find_or_initialize_by_uid(auth["uid"])
+    user.update_attributes!({
+      :name => auth["info"]["name"],
+      :nickname => auth["info"]["nickname"],
+      :email => auth["info"]["email"],
+      :avatar_url => auth["info"]["image"],
+    })
+
+    session[:user_id] = user.id
+    redirect '/'
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.find(session[:user_id])  if session[:user_id]
+    end
+  end
+end
